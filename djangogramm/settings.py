e43ed894750b5821aa76a7c4dc55c7006aea7505
Daniel Kravchenko <daniel.kravchenko@protonmail.com>
2025-07-27 16:57:23 +0300

    - chaged email backend.

--- djangogramm/settings.py
+++ djangogramm/settings.py
@@ -131,7 +131,7 @@ LOGIN_REDIRECT_URL = "home"
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
