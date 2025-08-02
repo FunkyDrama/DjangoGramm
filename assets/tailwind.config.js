756a0053e35edcd55d8d194c5bb95943b12ce93a
Daniel Kravchenko <daniel.kravchenko@protonmail.com>
2025-08-02 21:45:55 +0300

    - added AJAX (infinite scroll sentinel, follow/unfollow, reactions); - added webpack; - removed django-tailwind; - statics collected and added whitenoise and new template tag.

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../templates/**/*.html",
+    "../**/templates/**/*.html",
+    "./src/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
